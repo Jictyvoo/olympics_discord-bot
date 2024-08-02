b88553485923330aa71dc53f3be835c42374ca98
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-08-02 00:53:08 -0300

    :bell: feat: Refactor entire EventNotifier to manage its own notify jobs

--- internal/domain/services/cron_state.go
+++ internal/domain/services/cron_state.go
@@ -1,12 +1,8 @@
 package services
 
 import (
-	"errors"
 	"sync"
-	"time"
-
-	"github.com/go-co-op/gocron/v2"
-	"github.com/google/uuid"
+	"sync/atomic"
 
 	"github.com/jictyvoo/olympics_data_fetcher/internal/entities"
 )
@@ -15,41 +11,16 @@ type (
 	EventObserver interface {
 		OnEvent(event entities.OlympicEvent)
 	}
-	jobContext struct {
-		ID      uuid.UUID
-		startAt time.Time
-		endAt   time.Time
-	}
 
 	cronState struct {
-		cronScheduler       gocron.Scheduler
-		jobIDs              map[string]jobContext
 		registeredObservers []EventObserver
 		mutex               sync.Mutex
+		started             atomic.Bool
 	}
 )
 
-func (cs *cronState) retrieveJobID(key string) (jobContext, bool) {
-	if cs.jobIDs == nil {
-		return jobContext{ID: uuid.Nil}, false
-	}
-
-	c, ok := cs.jobIDs[key]
-	return c, ok
-}
-
-func (cs *cronState) registerJobInfo(key string, jCtx jobContext) {
-	cs.jobIDs[key] = jCtx
-}
-
-func (cs *cronState) removeJob(eventKey string, jobID uuid.UUID) error {
-	delete(cs.jobIDs, eventKey)
-	if err := cs.cronScheduler.RemoveJob(jobID); err != nil &&
-		!errors.Is(err, gocron.ErrJobNotFound) {
-		return err
-	}
-
-	return nil
+func (cs *cronState) Start() {
+	cs.started.Store(true)
 }
 
 func (cs *cronState) RegisterObserver(observer EventObserver) {
@@ -59,11 +30,17 @@ func (cs *cronState) RegisterObserver(observer EventObserver) {
 	cs.registeredObservers = append(cs.registeredObservers, observer)
 }
 
-func (cs *cronState) taskExecution(event entities.OlympicEvent) {
+func (cs *cronState) taskExecution(event entities.OlympicEvent) bool {
+	if !cs.started.Load() {
+		return false
+	}
+
 	cs.mutex.Lock()
 	defer cs.mutex.Unlock()
 
 	for _, observer := range cs.registeredObservers {
 		observer.OnEvent(event)
 	}
+
+	return true
 }
--- internal/domain/services/event_notifier.go
+++ internal/domain/services/event_notifier.go
@@ -6,9 +6,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/go-co-op/gocron/v2"
-	"github.com/google/uuid"
-
 	"github.com/jictyvoo/olympics_data_fetcher/internal/domain/usecases"
 	"github.com/jictyvoo/olympics_data_fetcher/internal/entities"
 )
@@ -22,49 +19,50 @@ type (
 
 	EventNotifierRepository interface {
 		EventLoader
-		CheckSentNotifications(
-			eventID entities.Identifier, eventChecksum string,
-		) (entities.Notification, error)
-		RegisterNotification(notification entities.Notification) error
+		usecases.CanNotifyRepository
 	}
 )
 
 type (
-	CancelChannel chan struct{}
+	CancelChannel    chan struct{}
+	notifierUseCases struct {
+		usecases.CanNotifyUseCase
+		usecases.FetcherCacheUseCase
+	}
 	EventNotifier struct {
-		cancelChan     CancelChannel
-		cacheDuration  time.Duration
-		fetcherUseCase usecases.FetcherCacheUseCase
-		repo           EventNotifierRepository
-		mutex          sync.Mutex
+		cancelChan      CancelChannel
+		checkInterval   time.Duration
+		olympicsEndDate time.Time
+		useCases        notifierUseCases
+		repo            EventNotifierRepository
+		mutex           sync.Mutex
 		cronState
 	}
 )
 
 func NewEventNotifier(
 	cancelChan CancelChannel, cacheDuration time.Duration,
-	repo EventNotifierRepository, fetcherUseCase usecases.FetcherCacheUseCase,
+	repo EventNotifierRepository,
+	fetcherUseCase usecases.FetcherCacheUseCase, canNotifyUseCase usecases.CanNotifyUseCase,
 ) (en *EventNotifier, err error) {
 	en = &EventNotifier{
-		cancelChan:     cancelChan,
-		cacheDuration:  cacheDuration,
-		repo:           repo,
-		fetcherUseCase: fetcherUseCase,
+		cancelChan:    cancelChan,
+		checkInterval: cacheDuration,
+		repo:          repo,
+		useCases: notifierUseCases{
+			CanNotifyUseCase:    canNotifyUseCase,
+			FetcherCacheUseCase: fetcherUseCase,
+		},
+		olympicsEndDate: time.Date(2024, time.August, 12, 0, 0, 0, 0, time.UTC),
 	}
-	en.cronScheduler, err = gocron.NewScheduler()
-	en.jobIDs = make(map[string]jobContext, 100)
 
 	return
-}
-
-func (en *EventNotifier) Start() {
-	en.cronScheduler.Start()
 }
 
 func (en *EventNotifier) MainLoop() error {
 	go en.fetcherThread()
 
-	ticker := time.NewTicker(en.cacheDuration >> 1)
+	ticker := time.NewTicker(en.checkInterval >> 1)
 	defer ticker.Stop()
 
 	// Do a first check before running the timer
@@ -91,7 +89,7 @@ func (en *EventNotifier) updateDisciplines() error {
 	en.mutex.Lock()
 	defer en.mutex.Unlock()
 
-	if err := en.fetcherUseCase.FetchDisciplines(); err != nil {
+	if err := en.useCases.FetchDisciplines(); err != nil {
 		return err
 	}
 	return nil
@@ -102,10 +100,14 @@ func (en *EventNotifier) fetchRemainingDays(from time.Time) {
 	defer en.mutex.Unlock()
 
 	startDate := time.Date(from.Year(), from.Month(), from.Day(), 0, 0, 0, 0, time.UTC)
-	endDate := time.Date(2024, time.August, 12, 0, 0, 0, 0, time.UTC)
+	endDate := startDate.Add(48 * time.Hour)
+	if endDate.After(en.olympicsEndDate) {
+		endDate = en.olympicsEndDate
+	}
+
 	for date := startDate; date.Before(endDate); date = date.Add(24 * time.Hour) {
 		slog.Info("Start to fetch and save data for event", slog.Time("date", date))
-		if err := en.fetcherUseCase.FetchDay(date); err != nil {
+		if err := en.useCases.FetcherCacheUseCase.FetchDay(date); err != nil {
 			slog.Error("Error fetching data from day", slog.String("error", err.Error()))
 		}
 	}
@@ -114,7 +116,7 @@ func (en *EventNotifier) fetchRemainingDays(from time.Time) {
 func (en *EventNotifier) fetcherThread() {
 	// Run one time since beginning
 	en.fetchRemainingDays(time.Date(2024, time.July, 24, 0, 0, 0, 0, time.UTC))
-	ticker := time.NewTicker(en.cacheDuration)
+	ticker := time.NewTicker(en.checkInterval)
 	for {
 		select {
 		case _, _ = <-en.cancelChan:
@@ -127,88 +129,25 @@ func (en *EventNotifier) fetcherThread() {
 }
 
 func (en *EventNotifier) taskExecution(event entities.OlympicEvent) {
-	en.mutex.Lock()
-	defer en.mutex.Unlock()
-
-	updatedEvent, err := en.repo.LoadEvent(event.ID)
-	if err != nil {
-		slog.Error(
-			"Error loading event",
-			slog.String("error", err.Error()),
-		)
-		updatedEvent = event
-	}
-
 	// Use cron state to trigger observers
-	en.cronState.taskExecution(updatedEvent)
+	notifyStatus := entities.NotificationStatusSent
+	if !en.cronState.taskExecution(event) {
+		notifyStatus = entities.NotificationStatusFailed
+	}
 
 	// Update status on database
 	newNotification := entities.Notification{
 		EventID:       event.ID,
-		Status:        entities.NotificationStatusSent,
+		Status:        notifyStatus,
 		EventChecksum: event.SHAIdentifier(),
 		NotifiedAt:    time.Now(),
 	}
-	err = en.repo.RegisterNotification(newNotification)
-	if err != nil {
+	if err := en.repo.RegisterNotification(newNotification); err != nil {
 		slog.Error(
 			"Error registering notification",
 			slog.String("error", err.Error()),
 		)
 	}
-}
-
-func (en *EventNotifier) manageEventJob(event entities.OlympicEvent) (err error) {
-	notifyID, checkErr := en.shouldNotify(event)
-	if notifyID == "" || checkErr != nil {
-		return checkErr
-	}
-
-	now := time.Now()
-	startTime := event.StartAt.Add(-20 * time.Minute)
-	if startTime.Before(now) {
-		startTime = now.Add(10 * time.Second)
-	}
-	newJob, insertErr := en.cronScheduler.NewJob(
-		gocron.OneTimeJob(
-			gocron.OneTimeJobStartDateTime(startTime),
-		),
-		gocron.NewTask(en.taskExecution, event),
-		// gocron.WithStopAt(gocron.WithStopDateTime(event.EndAt)),
-		gocron.WithLimitedRuns(1),
-		gocron.WithSingletonMode(gocron.LimitModeReschedule),
-	)
-
-	if insertErr != nil {
-		slog.Error("Error creating job", slog.String("error", insertErr.Error()))
-		return insertErr
-	}
-	en.cronState.registerJobInfo(
-		notifyID, jobContext{
-			ID:      newJob.ID(),
-			startAt: event.StartAt,
-			endAt:   event.EndAt,
-		},
-	)
-
-	err = en.repo.RegisterNotification(
-		entities.Notification{
-			EventID: event.ID, Status: entities.NotificationStatusPending, EventChecksum: event.SHAIdentifier(),
-		},
-	)
-
-	errStr := ""
-	if err != nil {
-		errStr = err.Error()
-	}
-	slog.Info(
-		"Job for notification registered",
-		slog.String("jobName", newJob.Name()),
-		slog.String("eventHash", event.SHAIdentifier()),
-		slog.String("dbErr", errStr),
-		slog.Time("startTime", startTime),
-	)
-	return
 }
 
 func (en *EventNotifier) checkUpdateJobs() error {
@@ -226,58 +165,26 @@ func (en *EventNotifier) checkUpdateJobs() error {
 	for _, event := range dayEvents {
 		// event.Competitors, err = en.repo.LoadCompetitorsFromEvent(event)
 
-		if err = en.manageEventJob(event); err != nil {
-			errList = append(errList, err)
+		eventKey, checkErr := en.useCases.ShouldNotify(event)
+		if checkErr != nil || eventKey == "" {
+			errList = append(errList, checkErr)
+			continue
 		}
+
+		// Check again for the 20min notification
+		/*startDiff := utils.AbsoluteNum(event.StartAt.Sub(time.Now()))
+		if startDiff <= 20*time.Minute {
+			en.taskExecution(event)
+		}*/
+		en.taskExecution(event)
+
+		slog.Info(
+			"Job for notification sent",
+			slog.String("eventHash", event.SHAIdentifier()),
+			slog.Time("startTime", event.StartAt),
+		)
+
 	}
 
 	return errors.Join(errList...)
-}
-
-func (en *EventNotifier) shouldNotify(event entities.OlympicEvent) (string, error) {
-	eventKey := event.SHAIdentifier()
-	jobCtx, found := en.cronState.retrieveJobID(eventKey)
-	if !found {
-		// Check if it exists on database
-		notificationRegister, err := en.repo.CheckSentNotifications(event.ID, eventKey)
-		if err == nil && notificationRegister.ID != 0 {
-			// Check if it has the pending status
-			if notificationRegister.Status != entities.NotificationStatusPending {
-				return "", nil
-			}
-		}
-
-		// Liberate for next checks
-	}
-
-	shouldInsertNew := true
-	if found && jobCtx.ID != uuid.Nil {
-		// Check if it has some update
-		shouldInsertNew = event.StartAt.Compare(jobCtx.startAt) != 0 ||
-			event.EndAt.Compare(jobCtx.endAt) != 0
-	}
-
-	updateStatus := entities.NotificationStatusSkipped
-	if !shouldInsertNew {
-		if err := en.cronState.removeJob(eventKey, jobCtx.ID); err != nil {
-			slog.Error("Error removing job", slog.String("error", err.Error()))
-			return "", err
-		}
-
-		updateStatus = entities.NotificationStatusCancelled
-	}
-
-	now := time.Now()
-	if shouldInsertNew && event.EndAt.After(now) {
-		return eventKey, nil
-	}
-
-	err := en.repo.RegisterNotification(
-		entities.Notification{
-			EventID:       event.ID,
-			Status:        updateStatus,
-			EventChecksum: event.SHAIdentifier(),
-		},
-	)
-	return "", err
 }
