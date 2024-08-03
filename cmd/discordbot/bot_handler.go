26ccdba7d1ed6f3629f279ab479729919674394e
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-08-03 02:51:31 -0300

    :pig: feat: [wip] Temporarily create a new bot handler for incoming msgs

--- cmd/discordbot/bot_handler.go
+++ cmd/discordbot/bot_handler.go
@@ -56,3 +56,21 @@ func (bot *OlympicsBot) ReadyHandler(s *discordgo.Session, r *discordgo.Ready) {
 	// Start scheduler
 	bot.evtNotifier.Start()
 }
+
+func (bot *OlympicsBot) MessagesHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	/*_, err := s.ChannelMessageSend(
+		m.ChannelID, "Future feature, isn't implemented yet.",
+	)
+	if err != nil {
+		slog.Error(
+			"Error sending message",
+			slog.String("err", err.Error()),
+		)
+	}*/
+}
